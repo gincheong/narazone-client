195deb9b7abf8c1b312ff9396f230e0ad5939f58
gincheong <gincheong2@gmail.com>
2021-08-30 23:41:20 +0900

    feat(BaseStyles): radius 속성 추가, 모바일 클릭 하이라이트 제거 추가

--- src/assets/styles/jss/BaseStyles.ts
+++ src/assets/styles/jss/BaseStyles.ts
@@ -13,7 +13,17 @@ const BaseStyle = {
   },
   shadow: {
     default: '0px 0px 10px rgba(0, 0, 0, 0.4)',
-  }
+  },
+  radius: {
+    default: '10px',
+  },
+  disableButtonHighlight: {
+    // 모바일에서, 클릭 시 파란색 highlight 효과 제거
+    '-webkit-tap-highlight-color': 'transparent',
+    outline: 'none',
+    '-ms-touch-action': 'manipulation',
+    'touch-action': 'manipulation',
+  },
 };
 
 export default BaseStyle;
