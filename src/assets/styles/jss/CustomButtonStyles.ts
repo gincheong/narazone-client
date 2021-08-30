3b1e2484dcd5e5a02725ec662514ebb5d74d1bff
gincheong <gincheong2@gmail.com>
2021-08-30 23:41:49 +0900

    feat(CustomButtonStyles): 모바일 클릭 하이라이트 제거

--- src/assets/styles/jss/CustomButtonStyles.ts
+++ src/assets/styles/jss/CustomButtonStyles.ts
@@ -1,20 +1,16 @@
 import { makeStyles } from '@material-ui/core';
+import BaseStyle from './BaseStyles';
 
 const useStyles = makeStyles({
   container: {
     padding: '20px',
-    margin: '15px',
-    borderRadius: '10px',
+    borderRadius: BaseStyle.radius.default,
     textDecoration: 'none',
     position: 'relative',
     overflow: 'hidden',
     cursor: 'pointer',
 
-    // 모바일에서, 클릭 시 파란색 highlight 효과 제거
-    '-webkit-tap-highlight-color': 'transparent',
-    outline: 'none',
-    '-ms-touch-action': 'manipulation',
-    'touch-action': 'manipulation',
+    ...BaseStyle.disableButtonHighlight,
   },
 });
 
